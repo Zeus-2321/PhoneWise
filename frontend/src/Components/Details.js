dda89fe7c71f6228885cbb5dfc36474a753519ee
Dev Pratap <devpratap2321@gmail.com>
2023-03-25 23:38:57 +0530

    Carousel

--- frontend/src/Components/Details.js
+++ frontend/src/Components/Details.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { InfinitySpin } from 'react-loader-spinner';
+import { Carousel } from 'react-bootstrap';
 import './Details.css';
 
 
@@ -31,7 +32,18 @@ function Details() {
         <div className="details-container">
             <div className="details-header">
                 <h2>{phone.brand} {phone.phone_name}</h2>
-                <img src={phone.phone_images[0]} alt={phone.phone_name} />
+                <Carousel slide={false} variant="dark" controls={false} interval={3000}>
+                    {phone.phone_images.map((image, index) => (
+                        <Carousel.Item key={index}>
+                            <img
+                                className="d-block w-100 mx-auto"
+                                src={image}
+                                alt={`Slide ${index}`}
+                                style={{ objectFit: 'contain' }}
+                            />
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
                 <p><strong>Release Date:</strong> {phone.release_date}</p>
                 <p><strong>Dimensions:</strong> {phone.dimension}</p>
                 <p><strong>OS:</strong> {phone.os}</p>
